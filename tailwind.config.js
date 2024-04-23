cb11c8e4838856fab91f65d030eb8cbe9d991c2e
pettik <petrbednarski@gmail.com>
2024-04-23 18:18:59 +0200

    commit HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,22 +9,23 @@ module.exports = {
   },
   theme: {
     extend: {
+      colors: {
+        darkBlue: 'hsl(217, 28%, 15%)',
+        darkBlue1: 'hsl(218, 28%, 13%)',
+        darkBlue2: 'hsl(216, 53%, 9%)',
+        darkBlue3: 'hsl(219, 30%, 18%)',
+        accentCyan: 'hsl(176, 68%, 64%)',
+        accentBlue: 'hsl(198, 60%, 50%)',
+        lightRed: 'hsl(0, 100%, 63%)',
+      },
       fontFamily: {
         sans: ['Raleway', 'sans-serif'],
         opensans: ['Open Sans', 'sans-serif'],
       },
-      colors: {
-        darkBlue: 'hsl(217,28%,15%)',
-        darkBlue1: 'hsl(218,28%,13%)',
-        darkBlue2: 'hsl(218,53%,9%)',
-        darkBlue3: 'hsl(218,30%,18%)',
-        accentCyan: 'hsl(176, 68%, 64%)',
-        lightRed: 'hsl(0,100%,63%)',
-      },
-      backgroundImage: (theme) => ({
+      backgroundImage: theme => ({
         'logo-dark-mode': "url('../images/logo-dark-mode.svg')",
         'logo-light-mode': "url('../images/logo-light-mode.svg')",
-        'curvy-dark-mode': "url('../images/bg-curvy-light-mode.svg')",
+        'curvy-dark-mode': "url('../images/bg-curvy-dark-mode.svg')",
         'curvy-light-mode': "url('../images/bg-curvy-light-mode.svg')",
       }),
     },
